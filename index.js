9c41f757bb379c943cc3d1f1df2cb3c2f6e99d72
syllesly <30665825+syllesly@users.noreply.github.com>
2018-10-07 20:20:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,35 +9,36 @@ var joinB;
 bot.on('message', (message) => {
     var n=message.guild.roles.find('name','Not Dangerous');
     const c=message.content;
+    var args=c.split(" ")
     if(c.includes("don\'t swear!")) {
         message.delete(2000);
     }
     if(message.author.bot) return;
-    if(c==prx+" invite") {
+    if(args[0]==prx && args[1]=="invite") {
         message.channel.send('Here '+message.author+":  https://discord.gg/nntvGP4");
     }
     if(message.author==join && c!=joinA+joinB+""){
         message.channel.send("No it is not "+join+".");
         join=0;
     }
-    if(c.startsWith(prx+" join")) {
+    if(args[0]==prx && args[1]=="join") {
         join=message.author;
         joinA=Math.floor(Math.random()*20);
         joinB=Math.floor(Math.random()*20);
         message.channel.send("Ok then "+join+", what is "+joinA+"+"+joinB+"?");
     }
-    if(c==joinA+joinB+"" && message.author==join){
+    if(args[0]==joinA+joinB+"" && message.author==join){
         message.channel.send("Welcome "+join+"!");
         message.member.addRole(n);
         join=0;
     }
-    if(c==prx+' ping'){
+    if(args[0]==prx && args[1]=='ping'){
         message.channel.send('pong');
     }
-    if(c==prx+' pong'){
+    if(args[0]==prx && args[1]=='pong'){
         message.channel.send('ping');
     }
-    if(c.startsWith(prx+" echo")) {
+    if(args[0]==prx && args[1]='echo') {
         var echo="";
         for(var i=(prx+" echo").length;i<c.length;i++) {
             echo+=c[i];
@@ -55,6 +56,13 @@ bot.on('message', (message) => {
             }
             message.channel.send("Hey "+message.author+" don\'t swear!");
         }
+    }
+    if(args[0]==prx && args[1]=="math") {
+        var s="";
+        for(var i=2;i<args.length;i++) {
+            s+=args[i];
+        }
+        message.channel.send(eval(s));
     }
 })
 bot.login(process.env.token);
